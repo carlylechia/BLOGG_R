042fe49962183b603505c954c221dc76bbed6a81
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-11 18:20:04 +0100

    Add method to count comments, use callbacks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_counter
+
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_count)
+  end
 end
