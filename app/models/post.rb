0bcaaf5d789174f6d323ffddda5a3348659bdb40
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-11 18:08:46 +0100

    Add the update_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
+  after_save :update_post_counter
+
   belongs_to :user, counter_cache: true
   has_many :comments
   has_many :likes
 
   def last_five_comments
     comments.limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def update_post_counter
+    user.increment!(:posts_count)
   end
 end
