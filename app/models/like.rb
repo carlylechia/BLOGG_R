806064cac85193029bbd0f18a7d602db4765f7d5
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-11 18:27:11 +0100

    Add method to count likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
+  after_save :update_likes_counter
+
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_count)
+  end
 end
