cd5047ff91e1ffc6ef1e9afa53eedbce2a9de612
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-12 13:12:48 +0100

    Add validations to post model to make tests pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   after_save :update_post_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
+
   belongs_to :user, counter_cache: true
   has_many :comments
   has_many :likes
