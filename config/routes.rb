8ae418730aff9f04439a765fed999f04683f46e3
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-15 18:24:37 +0100

    Add route for posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'pages#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  resources 'users', only: %w[index show] do
+    resources 'posts', only: %w[index]
+  end
 end
