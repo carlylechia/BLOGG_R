2f4ef8190b6280f92f54fea14f4429aa2f8b7ed1
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-15 16:48:18 +0100

    Add /users route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'pages#index'
+  get '/users' to 'users#index'
 end
