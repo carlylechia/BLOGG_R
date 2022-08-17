7848a1298342bbf5844f38e8b4ff20b96824c120
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-17 21:04:44 +0100

    Add view for the post index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user_id = params[:user_id]
+    @user = User.find(@user_id)
+    @posts = @user.posts.includes(:comments)
+  end
 
   def show
     @post_id = params[:id]
