46f51fc3b7b88a5b185b08d51bf73f4730d34248
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-17 16:21:58 +0100

    Set the default route to render users index view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'pages#index'
+  get '/', to: 'users#index'
   resources 'users', only: %w[index show] do
     resources 'posts', only: %w[index show]
   end
