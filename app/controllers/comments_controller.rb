5d79e8bc7f91420a4a66595e31621d9502dcbeb8
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-18 22:56:41 +0100

    Add action to create comment in CommentController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+        flash[:success] = 'Comment created successfully!'
+      else
+        flash.now[:error] = 'Error: Failed to save comment, please try again.'
+        format.html { redirect_to user_post_path(current_user, @post) }
+      end
+    end
+  end
+
   private
 
   def comment_params
