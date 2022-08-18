a4b098dc06849a76cb7d1cbb6e1b68071bd83d81
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-19 00:14:36 +0100

    Add a new comment to a post by filling a form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,9 +15,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
+    @post = current_user.posts.build(post_params)
     if @post.save
+      flash[:success] = 'Post created successfully'
       redirect_to user_posts_path(current_user)
     else
       render :new
