8f720f508e971b3e362dce34c0614797294bd64f
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-18 22:23:48 +0100

    Render form which creates new posts in a new route

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  after_save :update_post_counter
+  # after_save :update_post_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
