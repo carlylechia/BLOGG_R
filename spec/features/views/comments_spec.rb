2c30c81e86a94418242bb1723be437c594c6c0d2
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-23 14:09:45 +0100

    Make adjustments to specs

--- spec/features/views/comments_spec.rb
+++ spec/features/views/comments_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :view, js: true do
   before :each do
-    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @user = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                         posts_count: 0)
     @user.posts.create(title: 'Title', text: 'Body')
   end
 
   it 'Should display comment counter' do
-    visit root_path
+    visit users_path
     click_link 'Tom'
     expect(page).to have_content('See all posts')
   end
