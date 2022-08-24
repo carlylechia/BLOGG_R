dffb9e20879a288b8baedf8bdcd5295c116e3b68
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-24 23:53:17 +0100

    Solve migration issues

--- db/migrate/20220824174034_add_confirmable_to_devise.rb
+++
@@ -1,8 +0,0 @@
-class AddConfirmableToDevise < ActiveRecord::Migration[6.1]
-  def change
-    change_table(:users) do |t| 
-      t.confirmable 
-    end
-    add_index  :users, :confirmation_token, :unique => true 
-  end
-end
