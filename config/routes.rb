7ccdafa8f6291ced0357e3684eba397e9804ac33
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-26 12:36:45 +0100

    Configure and setup devise_jwt and cors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,24 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+
+  # devise
+  devise_for :users,
+  # controllers: {
+  #     sessions: 'users/sessions',
+  #     registrations: 'users/registrations'
+  # }
+
+  resources 'users', only: %w[index show] do
+    resources 'posts', only: %w[index show new]
+  end
+  resources :posts, only: [:new, :create, :destroy] do
+    resources :comments
+    resources :likes
+  end
+
+  # API routes
   namespace :api, :defaults => {:format => :json} do
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :show, :new]
@@ -10,17 +28,5 @@ Rails.application.routes.draw do
       resources :comments
       resources :likes
     end
-  end
-
-  root 'users#index'
-
-  devise_for :users
-
-  resources 'users', only: %w[index show] do
-    resources 'posts', only: %w[index show new]
-  end
-  resources :posts, only: [:new, :create, :destroy] do
-    resources :comments
-    resources :likes
   end
 end
