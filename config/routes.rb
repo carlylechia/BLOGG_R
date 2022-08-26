fbb7fc17c04352c2f0da09ff90fcd0171728a8c0
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-26 12:00:41 +0100

    BugFix: Catch and fix the bug in the comment model

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, :defaults => {:format => :json} do
-    resources :users do
-      resources :posts
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show, :new]
     end
     resources :posts, only: [:new, :create, :destroy] do
       resources :comments
